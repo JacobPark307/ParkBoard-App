1dfa7463711624cedebd1dcade38dd6ca472d2a4
Jacob Velasquez <Jacob@parkboard.info>
2024-12-19 14:11:45 -0700

    Baby changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,50 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="container">
+	<div class="header">
+		<h2>City Of Laramie Parks Dept</h2>
+		<button>Page 1</button>
+		<button>Page 2</button>
+		<button>Dark</button>
+	</div>
+
+	<div class="issues">
+		<div class="Problems">
+			<h3>Issues and Problems</h3>
+			<p class="editable" contenteditable="true" id="issue1">System 1 Pump Issues: Motor On order</p>
+			<p class="editable" contenteditable="true" id="issue2">Order fittings and pipe</p>
+		</div>
+
+		<div class="system-section">
+			<div class="system system-1">
+				<div class="system-title">System 1</div>
+				<p class="editable" contenteditable="true" id="system1_status1">System Off</p>
+				<p class="editable" contenteditable="true" id="system1_status2">Program set</p>
+				<p class="editable" contenteditable="true" id="system1_status3">Pump issue</p>
+				<p class="editable" contenteditable="true" id="system1_status4">Zone 3 no talk</p>
+				<p class="editable" contenteditable="true" id="system1_status5">Not Adjusted</p>
+			</div>
+			<div class="system system-2">
+				<div class="system-title">System 2</div>
+				<p class="editable" contenteditable="true" id="system2_status1">System On</p>
+				<p class="editable" contenteditable="true" id="system2_status2">Program Set</p>
+				<p class="editable" contenteditable="true" id="system2_status3">Not Adjusted</p>
+			</div>
+			<div class="system system-3">
+				<div class="system-title">System 3</div>
+				<p class="editable" contenteditable="true" id="system3_status1">System On</p>
+				<p class="editable" contenteditable="true" id="system3_status2">Program Set</p>
+				<p class="editable" contenteditable="true" id="system3_status3">Adjusted</p>
+			</div>
+		</div>
+	</div>
+
+	<div class="notes">
+		<div>
+			<h3>Notes</h3>
+			<p class="editable" contenteditable="true" id="note1">Need to file receipt by Friday 8/5</p>
+			<p class="editable" contenteditable="true" id="note2">Pay day is Monday</p>
+			<p class="editable" contenteditable="true" id="note3">Someone needs to check vaults for water</p>
+			<p class="editable" contenteditable="true" id="note4">Contact contractors about new pump housing</p>
+		</div>
+	</div>
+</div>
+
